3ab015faed2bd94b90cef82858442b2b881ba382
Secret297 <73541046+Secret297-CODER-SOURCE@users.noreply.github.com>
2025-01-10 18:57:11 +0300

    V 0.5.1 Beta

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import __logging__ as log
 
 logger=log.Logger()
 bot = telebot.TeleBot("7852439023:AAHhHkQ-vli6qJhytR-zJl9FOIgWVogfk0c")
+# Bot user name @name_itfriends_bot
 @bot.message_handler(content_types=['text'])
 def Assistant_Exchange(message):
     if message.text=="/start":
